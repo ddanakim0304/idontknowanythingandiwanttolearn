2fa4a8a3e3c8a97338b415cd6d2884e27c31c033
Dain <kim@uni.minerva.edu>
2025-06-30 14:36:16 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-import NewResultsPage from './components/NewResultsPage';
+import ResultsPage from './components/ResultsPage';
 import InsightsPage from './components/InsightsPage';
 
-function App() {
-  return (
-    <div className="min-h-screen bg-warm-bg font-sans">
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/results" element={<NewResultsPage />} />
-          <Route path="/insights" element={<InsightsPage />} />
-        </Routes>
-      </Router>
     </div>
   );
 }
