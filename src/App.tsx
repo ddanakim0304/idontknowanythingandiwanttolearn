a392f897748130ade40da6f58a9d7e777fd42432
Dain <kim@uni.minerva.edu>
2025-06-30 14:39:16 -0500

    Fix App.tsx component structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,17 @@ import LandingPage from './components/LandingPage';
 import ResultsPage from './components/ResultsPage';
 import InsightsPage from './components/InsightsPage';
 
-    </div>
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/results" element={<ResultsPage />} />
+          <Route path="/insights" element={<InsightsPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
